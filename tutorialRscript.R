fea4c2add6f5663fe4890d4e97cebc644e2199be
Šárka Ondrouchová <ondrouchovas@seznam.cz>
2021-12-09 20:33:02 +0000

    sharka updated

---
+++ tutorialRscript.R
@@ -0,0 +1,14 @@
+# Sarka Ondrouchova
+# s1861053@ed.ac.uk
+# Efficient coding in RStudio Tutorial
+# December 2021
+
+# Set working directory ----
+setwd("C:/Users/ondro/OneDrive/School/Data Science/tutorial-sharkie58")
+
+# Load libraries ----
+# install.packages("readxl")
+library("readxl")
+
+# Load files ----
+impala <- read_excel("tameimpala.xlsx")
